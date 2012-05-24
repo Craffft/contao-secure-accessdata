8a04813944fc778fce4449cd9f03458ecb7e63b2
iCodr8 <icodr8@googlemail.com>
2012-05-24 18:28:40 +0200

    added runonce
    run once to upgrade to 1.1.0

---
+++ config/_runonce.php
@@ -0,0 +1,64 @@
+<?php
+
+class SecureAccessdataRunonce extends Controller
+{
+	/**
+	 * Initialize the object
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+
+		// Fix potential Exception on line 0 because of __destruct method (see http://dev.contao.org/issues/2236)
+		$this->import((TL_MODE=='BE' ? 'BackendUser' : 'FrontendUser'), 'User');
+		$this->import('Database');
+		$this->import('Encryption');
+	}
+
+	/**
+	 * Execute all runonce files in module config directories
+	 */
+	public function run()
+	{
+		$this->upgrade_to_1_1_0();
+		
+		/*$this->import('Files');
+		$arrModules = scan(TL_ROOT . '/system/modules/');
+
+		foreach ($arrModules as $strModule)
+		{
+			if ((@include(TL_ROOT . '/system/modules/' . $strModule . '/config/runonce.php')) !== false)
+			{
+				$this->Files->delete('system/modules/' . $strModule . '/config/runonce.php');
+			}
+		}*/
+	}
+	
+	
+	private function upgrade_to_1_1_0()
+	{
+		// Read fields decrypt them an save them
+		$objData = $this->Database->prepare("SELECT id, access_title, author FROM tl_secure_accessdata")
+								  ->execute();
+		
+		while($objData->next())
+		{
+			$arrSet = array();
+			$arrSet['access_title'] = ($this->Encryption->decrypt($objData->access_title) == '') ? $objData->access_title : $this->Encryption->decrypt($objData->access_title);
+			$arrSet['author'] = ($this->Encryption->decrypt($objData->author) == '') ? $objData->author : $this->Encryption->decrypt($objData->author);
+			
+			$insertElement = $this->Database->prepare("INSERT INTO tl_secure_accessdata %s WHERE id=?")
+											->set($arrSet)
+											->execute($objData->id);
+		}
+	}
+}
+
+
+/**
+ * Instantiate controller
+ */
+$objSecureAccessdataRunonce = new SecureAccessdataRunonce();
+$objSecureAccessdataRunonce->run();
+
+?>
