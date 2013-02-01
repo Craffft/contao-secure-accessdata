199a6e7f419a1ee116db5703b0548d1a8e507e11
iCodr8 <icodr8@gmail.com>
2013-02-01 19:03:54 +0100

    added model

---
+++ models/SecureAccessdataModel.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (C) 2005-2013 Leo Feyer
+ *
+ * @package   photoalbums2
+ * @author    Daniel Kiesel <https://github.com/icodr8>
+ * @license   LGPL
+ * @copyright Daniel Kiesel 2011-2013
+ */
+
+
+/**
+ * Namespace
+ */
+namespace SecureAccessdata;
+
+/**
+ * Class SecureAccessdataModel
+ *
+ * @copyright  Daniel Kiesel 2011-2013
+ * @author     Daniel Kiesel <https://github.com/icodr8>
+ * @package    secure_accessdata
+ */
+class SecureAccessdataModel extends \Model
+{
+
+	/**
+	 * Name of the table
+	 * @var string
+	 */
+	protected static $strTable = 'tl_secure_accessdata';
+
+}
