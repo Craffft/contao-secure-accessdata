4098e6c6140b412b83ed3ae74a83d183a6a4cdc3
iCodr8 <icodr8@gmail.com>
2015-05-22 16:03:40 +0200

    fixed wrong named variable

--- system/modules/secure-accessdata/dca/tl_secure_accessdata.php
+++ system/modules/secure-accessdata/dca/tl_secure_accessdata.php
@@ -898,7 +898,7 @@ class tl_secure_accessdata extends \Backend
 						if ($this->User->id != $author)
 						{
 							// If not in user array
-							if (!(is_array($protect_users) && count($protected_users) < 1 && in_array($this->User->id, $protect_users)))
+							if (!(is_array($protect_users) && count($protect_users) < 1 && in_array($this->User->id, $protect_users)))
 							{
 								// If not in group array
 								if (!(is_array($this->User->groups) && is_array($protect_groups) && count(array_intersect($this->User->groups, $protect_groups)) > 0))
